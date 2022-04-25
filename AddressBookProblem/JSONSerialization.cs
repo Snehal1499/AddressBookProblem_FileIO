8d6402fbe8aa4ab200db9c91bc8219c9106c921e
Snehal1499 <snehalpatil1718@gmail.com>
2022-04-25 22:39:33 +0530

    Added UC13 JSON Problem

---
+++ AddressBookProblem/JSONSerialization.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    internal class JSONSerialization
+    {
+        public void JsonSerialize()
+        {
+            Blogsites bsObj = new Blogsites()
+            {
+                Name = "Snehal",
+                Description = "22 yr old Female"
+            };
+            //Converting Blogsite object to Jason string format
+            string jsonData = JsonConvert.SerializeObject(bsObj);
+            Console.WriteLine(jsonData);
+        }
+        public void JsonDesrialize()
+        {
+            string json = @"{ 'Name' : 'Snehal',
+                               'Description' : '22 yr old Female'}";
+            Blogsites bsobj = JsonConvert.DeserializeObject<Blogsites>(json);
+            Console.WriteLine(bsobj.Name);
+
+        }
+        [DataContract]
+        class Blogsites
+        {
+            [DataMember]
+            public string Name { get; set; }
+            [DataMember]
+            public string Description { get; set; }
+
+        }
+    }
+}
